e1c6316c9821373fcfbc3220273f5c21268a8b02
jenniferboedker <jennifer.boedker@qbic.uni-tuebingen.de>
2022-03-08 09:42:33 +0100

    Reformat code

--- src/main/java/life/qbic/samplecleaner/App.java
+++ src/main/java/life/qbic/samplecleaner/App.java
@@ -1,5 +1,14 @@
 package life.qbic.samplecleaner;
 
+import life.qbic.samplecleaner.tracking.SampleLocation;
+import life.qbic.samplecleaner.tracking.SampleLocationRepository;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -10,15 +19,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
-import life.qbic.samplecleaner.tracking.SampleLocation;
-import life.qbic.samplecleaner.tracking.SampleLocationRepository;
-import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-
 @SpringBootApplication
 public class App implements CommandLineRunner {
 
@@ -26,7 +26,7 @@ public class App implements CommandLineRunner {
 
   final SampleLocationRepository sampleLocationRepository;
 
-  final static Pattern sampleCodePattern = Pattern.compile("^Q.*");
+  static final Pattern sampleCodePattern = Pattern.compile("^Q.*");
 
   private static final Logger LOG = org.apache.logging.log4j.LogManager.getLogger(App.class);
 
@@ -54,25 +54,27 @@ public class App implements CommandLineRunner {
 
       var trackedSamples = sampleLocationRepository.getSampleLocations(projectCode);
       LOG.info(
-              String.format("Collected %d tracked samples for project %s ...", trackedSamples.size(),
-                      projectCode));
+          String.format(
+              "Collected %d tracked samples for project %s ...",
+              trackedSamples.size(), projectCode));
 
-      var samplesToDelete = extractSamplesToDelete(whiteListedSamples,trackedSamples);
+      var samplesToDelete = extractSamplesToDelete(whiteListedSamples, trackedSamples);
       deleteAllSamples(samplesToDelete);
-      LOG.info(String.format("Deleted %d samples...",samplesToDelete.size()));
+      LOG.info(String.format("Deleted %d samples...", samplesToDelete.size()));
 
     } catch (Exception e) {
       LOG.error(e.getMessage());
     }
   }
 
-  private List<SampleLocation> extractSamplesToDelete(HashSet<String> whiteListedSamples, List<SampleLocation> trackedSamples){
+  private List<SampleLocation> extractSamplesToDelete(
+      HashSet<String> whiteListedSamples, List<SampleLocation> trackedSamples) {
     return trackedSamples.stream()
-            .filter(sampleLocation -> !whiteListedSamples.contains(sampleLocation.getSampleId()))
-            .collect(Collectors.toList());
+        .filter(sampleLocation -> !whiteListedSamples.contains(sampleLocation.getSampleId()))
+        .collect(Collectors.toList());
   }
 
-  private void deleteAllSamples(List<SampleLocation> samples){
+  private void deleteAllSamples(List<SampleLocation> samples) {
     for (var sample : samples) {
       sampleLocationRepository.deleteSample(sample.getSampleId());
     }
